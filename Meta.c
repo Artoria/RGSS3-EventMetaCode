713325c1d9eb467989387609f522172b2ffd509d
Artoria <pochy2008@qq.com>
2013-11-05 13:28:03 +0800

    Update Meta.c

--- Meta.c
+++ Meta.c
@@ -1,4 +1,4 @@
-  cmd("显示文章", 101);
+cmd("显示文章", 101);
   start();
     entity("脸图", "string");
     entity("编号", "number");
@@ -38,7 +38,7 @@
   cmd("条件分歧", 111);
   start();
     selectcase("开关", "变量", "独立开关", "计时器", "角色", "敌人", "事件", "物品", "武器", "护甲", "按下按钮", "脚本", "载具", end_item());
-      start();
+
         casewhen(0); 
           start();
             entity("开关编号", "switch");
@@ -48,14 +48,9 @@
         casewhen(1);
           start();
             entity("变量编号", "var");
-            
             selectcase("常量", "变量", end_item());
-            start();
               casewhen(0); start(); entity("常量", "number"); stop(); 
               casewhen(1); start(); entity("变量", "var"); stop();
-            stop();
-            
-            
             enumcase("关系", "等于", "以上", "以下", "大于", "小于", "不等于", end_item());
           stop();
           
@@ -74,14 +69,12 @@
         casewhen(4);
         start();
            selectcase("在队伍时", "名字是", "职业是", "技能学会", "武器装备", "防具装备", "状态有", end_item());
-           start();
             casewhen(1); start(); entity("名字", "string"); stop();
             casewhen(2); start(); entity("职业", "class"); stop();
             casewhen(3); start(); entity("技能", "skill"); stop();
             casewhen(4); start(); entity("武器", "weapon"); stop();
             casewhen(5); start(); entity("防具", "armor"); stop();
             casewhen(6); start(); entity("状态", "state"); stop();
-           stop();
         stop();
         
         casewhen(5);
@@ -138,7 +131,7 @@
         stop();
         
         
-      stop();
+
   stop();
   
   cmd("调用公共事件", 117);
@@ -164,14 +157,12 @@
     entity("到", "var");
     enumcase("操作", "代入", "加上", "减去", "乘以", "除以", "取余", end_item());
     selectcase("常量", "变量", "随机数", "游戏数据", "脚本");
-    start();
       casewhen(0); start(); entity("常数", "number"); stop();      
       casewhen(1); start(); entity("变量", "var"); stop();
       casewhen(2); start(); entity("随机数", "number");entity("随机数", "number"); stop();
       casewhen(4); start(); entity("脚本", "string"); stop();
       casewhen(3); start();
          selectcase("物品数量", "武器数量", "防具数量", "角色", "敌人", "地图人物", "队伍", end_item());
-         start();
            casewhen(0); start(); entity("物品", "item"); stop();
            casewhen(1); start(); entity("武器", "weapon"); stop();
            casewhen(2); start(); entity("防具", "armor"); stop();
@@ -193,10 +184,8 @@
              enumcase("其他数据", "地图ID", "队伍人数", "金钱", "步数", "游戏时间", "计时器", "存档回数", "战斗回数", end_item());
            stop();
           
-         stop();
-      stop();
+      stop();    
     stop();
-   
     cmd("脚本", 355);
     start();
       entity("脚本", "string");
