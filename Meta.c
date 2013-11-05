dbb9b03d3a998b0d177795af23288c1e470dafa5
Artoria <pochy2008@qq.com>
2013-11-05 09:55:16 +0800

    Update Meta.c

--- Meta.c
+++ Meta.c
@@ -1,4 +1,4 @@
-cmd(111)
+cmd(111);
 start();
     selectcase("开关", "变量", "独立开关", "计时器", "角色", "敌人", "事件", "物品", "武器", "护甲", "按下按钮", "脚本", "载具", end_item());
       start();
@@ -10,15 +10,14 @@ start();
         
         casewhen(1);
           start();
-            varid();  #1
+            varid();  
             
-            selectcase("直接指定", "变量指定", end_item());#2
+            selectcase("直接指定", "变量指定", end_item());
             start();#3
               casewhen(0); start(); number(); stop(); 
               casewhen(1); start(); varid (); stop();
             stop();
             
-            #4
             enumcase("等于", "以上", "以下", "大于", "小于", "不等于", end_item());
             
           stop();
